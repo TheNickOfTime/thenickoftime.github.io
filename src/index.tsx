487d88b6b2199d4494ac5482897839e7f5143175
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-04-10 04:12:40 +0000

    Setting up primary pages, components, and packages

---
+++ src/index.tsx
@@ -0,0 +1,45 @@
+// Dependencies ------------------------------------------------------------------------------------
+
+// React
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Styles
+import './global.scss';
+import '@fontsource-variable/darker-grotesque';
+import '@fontsource-variable/victor-mono';
+import '@fontsource/material-icons';
+import '@fortawesome/react-fontawesome';
+
+//Components
+import Navigation from './components/navigation/navigation';
+
+// Pages
+import Home from './pages/home/home';
+import Projects from './pages/projects/projects';
+
+// Test pages
+import MarkdownTest from './pages/test/markdown/markdown';
+import StyleTest from './pages/test/style/style';
+
+// App ---------------------------------------------------------------------------------------------
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<main>
+			<Router>
+				<Routes>
+					<Route path='/' element={<Navigation />}>
+						{/* Index Page */}
+						<Route index element={<Home />} />
+						{/* Main Pages */}
+						<Route path='projects' element={<Projects />} />
+						{/* Test Pages */}
+						<Route path='markdown-test' element={<MarkdownTest />} />
+						<Route path='style-test' element={<StyleTest />} />
+					</Route>
+				</Routes>
+			</Router>
+		</main>
+	</React.StrictMode>
+);
