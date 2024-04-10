5d01032842219733d166da2992ce30393d381227
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-04-10 19:13:28 +0000

    Initial pass on loading data from .mdx files into project cards

--- src/components/project-card/project-card.tsx
+++ src/components/project-card/project-card.tsx
@@ -1,49 +1,69 @@
 import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDocker, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDocker, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons';
-import './project-card.scss'
+import './project-card.scss';
 
-export default function ProjectCard({project}: {project: string}) {
+const linkLookup = {
+	github: { icon: faGithub, label: 'View Source Code' },
+	codespace: { icon: faDocker, label: 'Open in Github Codespace' },
+};
+
+export default function ProjectCard({ projectName, projectThumb, projectLinks }) {
 	const [isExpanded, setExpanded] = useState(false);
 
 	const toggleQuicklinks = () => {
 		setExpanded(!isExpanded);
-	}
+	};
 
 	const renderQuicklinks = () => {
 		return (
-			<div className='project-quicklinks' >
-				<button className='quicklink-button'>
+			<div className='project-quicklinks'>
+				{Object.entries(projectLinks).map((entry) => {
+					const link = entry[1];
+					const linkData = linkLookup[entry[0]];
+					console.log(linkData);
+					return (
+						<a className='button quicklink-button' href={link} target='_blank'>
+							<FontAwesomeIcon className='quicklink-icon' icon={linkData.icon} />
+							<span className='quicklink-label'>{linkData.label}</span>
+						</a>
+					);
+				})}
+				{/* <button className='quicklink-button'>
 					<FontAwesomeIcon className='quicklink-icon' icon={faGithub} />
 					<span className='quicklink-label'>View Source Code</span>
 				</button>
 				<button className='quicklink-button'>
 					<FontAwesomeIcon className='quicklink-icon' icon={faDocker} />
 					<span className='quicklink-label'>Open in GitHub Codespace</span>
-				</button>
+				</button> */}
 			</div>
-		)
-	}
+		);
+	};
 
 	const renderDropdown = () => {
 		return (
 			<div className='project-quicklink-dropdown' onClick={toggleQuicklinks}>
 				<span className='quicklink-label'>{isExpanded ? 'See Less' : 'See More'}</span>
-				{isExpanded ? <FontAwesomeIcon icon={faCaretUp} /> : <FontAwesomeIcon icon={faCaretDown} />}
+				{isExpanded ? (
+					<FontAwesomeIcon icon={faCaretUp} />
+				) : (
+					<FontAwesomeIcon icon={faCaretDown} />
+				)}
 			</div>
-		)
-	}
+		);
+	};
 
 	return (
-		<div className="project-card elevated">
-			<img className='project-image' src="/thumbs/portfolio/choropleth-map.png" alt="" />
+		<div className='project-card elevated'>
+			<img className='project-image' src={projectThumb} alt='' />
 			<div className='project-info'>
-				<h3 className='project-name'>{project}</h3>
+				<h3 className='project-name'>{projectName}</h3>
 				<p className='project-description'>This is a description for a project.</p>
 			</div>
 			{isExpanded && renderQuicklinks()}
 			{renderDropdown()}
 		</div>
-	)
+	);
 }
--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -1,24 +1,26 @@
-import './projects.scss'
-import ProjectCard from "../../components/project-card/project-card"
+import './projects.scss';
+import ProjectCard from '../../components/project-card/project-card';
 
 export default function Projects() {
-
-	const projects = [
-		'Project 1',
-		'Project 2',
-		'Project 3'
-	]
+	const projects = import.meta.glob('/src/projects/portfolio/*.mdx', { eager: true });
+	console.log(projects);
 
 	return (
 		<div>
 			<h1>Projects</h1>
 			<div id='project-browser'>
 				<div id='projects'>
-					{projects.map((project) => {
-						return <ProjectCard project={project} />
+					{Object.values(projects).map((project) => {
+						return (
+							<ProjectCard
+								projectName={project.projectName}
+								projectThumb={project.projectThumb}
+								projectLinks={project.projectLinks}
+							/>
+						);
 					})}
 				</div>
 			</div>
 		</div>
-	)
+	);
 }
