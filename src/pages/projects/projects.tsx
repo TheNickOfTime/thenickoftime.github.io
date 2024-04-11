9b0febe731db31a7c7fd2ba66a8f81be7201c200
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-04-11 23:18:27 +0000

    Adding filtering to project browser.

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -1,27 +1,11 @@
 import './projects.scss';
-import ProjectCard from '../../components/project-card/project-card';
+import { ProjectBrowser } from './components/project-browser/project-browser';
 
 export default function Projects() {
-	const projects = import.meta.glob('/src/projects/portfolio/*.mdx', { eager: true });
-	// console.log(projects);
-
 	return (
 		<div>
 			<h1>Projects</h1>
-			<div id='project-browser'>
-				<div id='projects'>
-					{Object.values(projects).map((project) => {
-						return (
-							<ProjectCard
-								projectName={project.projectName}
-								projectThumb={project.projectThumb}
-								projectLinks={project.projectLinks}
-								projectTags={project.projectTags}
-							/>
-						);
-					})}
-				</div>
-			</div>
+			<ProjectBrowser />
 		</div>
 	);
 }
