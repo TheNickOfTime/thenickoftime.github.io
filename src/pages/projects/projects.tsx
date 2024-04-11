18738713d73a8d0d134f5cfde13cc5681483abbd
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-04-11 23:49:44 +0000

    Adding blurb to the projects page

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -5,6 +5,11 @@ export default function Projects() {
 	return (
 		<div>
 			<h1>Projects</h1>
+			<p>
+				Below you will find a list of <em>all</em> of the projects I have worked on, big &
+				small. For your convenience, please use the filtering and sorting options 😊.
+			</p>
+			<br />
 			<ProjectBrowser />
 		</div>
 	);
