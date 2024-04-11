0d628234e76ccaa3b7d20ae26588d33cf01c9444
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-04-11 22:46:43 +0000

    Removing front matter plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import mdx from '@mdx-js/rollup';
-import remarkFrontmatter from 'remark-frontmatter';
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
+// import remarkFrontmatter from 'remark-frontmatter';
+// import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), mdx({ remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter] })],
+	plugins: [react(), mdx()],
 	resolve: {
 		alias: {
 			src: '/src',
