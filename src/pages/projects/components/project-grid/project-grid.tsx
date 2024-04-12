c6cc3b57a96bbe4d7dfc2ce6d5a973593dab9995
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-04-12 21:23:27 +0000

    Adding sorting logic to the project browser

--- src/pages/projects/components/project-grid/project-grid.tsx
+++ src/pages/projects/components/project-grid/project-grid.tsx
@@ -5,30 +5,59 @@ interface Params {
 	projects: Projects;
 	tags: Tags;
 	setProjectCount: React.Dispatch<React.SetStateAction<number>>;
+	sortBy: string;
+	sortOrder: string;
 }
 
-export default function ProjectGrid({ projects, tags, setProjectCount }: Params) {
+export default function ProjectGrid({
+	projects,
+	setProjectCount,
+	tags,
+	sortBy,
+	sortOrder,
+}: Params) {
 	// console.log(Object.values(tags));
 
 	const shouldFilter = !Object.values(tags).every((tag) => !tag);
 
-	const unfilteredProjects = () => {
-		const projectsUnfiltered = Object.values(projects);
+	const sortAlphabetical = (projects: Project[]) => {
+		const sortedProjects = projects.sort((a, b) => {
+			const after = a.projectName.toLowerCase() > b.projectName.toLowerCase();
+			const before = a.projectName.toLowerCase() < b.projectName.toLowerCase();
 
-		setProjectCount(projectsUnfiltered.length);
+			return after ? 1 : before ? -1 : 0;
+		});
 
-		return projectsUnfiltered;
+		return sortOrder == 'Descending' ? sortedProjects : sortedProjects.reverse();
 	};
 
-	const filteredProjects = () => {
-		const projectsUnfiltered = Object.values(projects);
-		const tagsFiltered = projectsUnfiltered.filter((project) => {
+	const sortFeatured = (projects: Project[]) => {
+		return sortAlphabetical(projects);
+	};
+
+	const sortProjects = (projects: Project[]) => {
+		switch (sortBy) {
+			case 'Alphabetical':
+				return sortAlphabetical(projects);
+			case 'Featured':
+				return sortFeatured(projects);
+			default:
+				return sortAlphabetical(projects);
+		}
+	};
+
+	const unfilteredProjects = (projects: Project[]) => {
+		setProjectCount(projects.length);
+
+		return projects;
+	};
+
+	const filteredProjects = (projects: Project[]) => {
+		const projectsFiltered = projects.filter((project) => {
 			const currentTags = Object.keys(tags).filter((key) => tags[key]);
 			const projectInTags = project.projectTags.some((tag) => currentTags.includes(tag));
 			return projectInTags;
 		});
-
-		const projectsFiltered = tagsFiltered;
 
 		setProjectCount(projectsFiltered.length);
 
@@ -36,7 +65,11 @@ export default function ProjectGrid({ projects, tags, setProjectCount }: Params)
 	};
 
 	const Projects = () => {
-		const projectsToRender = shouldFilter ? filteredProjects() : unfilteredProjects();
+		let projectsToRender = Object.values(projects);
+		projectsToRender = sortProjects(projectsToRender);
+		projectsToRender = shouldFilter
+			? filteredProjects(projectsToRender)
+			: unfilteredProjects(projectsToRender);
 
 		return projectsToRender.map((project: Project) => {
 			return <ProjectCard project={project} key={project.projectName} />;
