87b5a7067068bc49cdb4436d8d9bea319e310f95
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-04-13 03:00:28 +0000

    Adding more featured projects

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -6,9 +6,9 @@ export default function Projects() {
 		<div>
 			<h1>Projects</h1>
 			<p>
-				Below lies <strong>the project browser</strong>. By default it will show featured
-				projects I want to highlight - if you're not seeing what you want, please use the{' '}
-				<em>filters</em> and <em>sorting</em> options to see more!
+				Below lies <strong>the project browser</strong>. By default it will show a limited
+				number of featured projects I want to highlight - if you're not seeing what you
+				want, please use the <em>filters</em> and <em>sorting</em> options to see more!
 			</p>
 			<br />
 			<ProjectBrowser />
