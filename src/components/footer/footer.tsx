4f8ec568e902e51a52b8f06db44855d141fd9d00
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-04-15 01:46:39 +0000

    Removing hero from footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,11 +3,11 @@ import './footer.scss';
 export default function Footer() {
 	return (
 		<div id='footer'>
-			<div id='footer-main'>
+			{/* <div id='footer-main'>
 				<div id='footer-hero'>
 					<img src='/hero.png' alt='Footer Hero' />
 				</div>
-			</div>
+			</div> */}
 			<div id='footer-copyright'>{new Date().getFullYear()} © Nick Cunningham</div>
 		</div>
 	);
