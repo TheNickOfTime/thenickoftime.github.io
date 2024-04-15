2f7ae9fb4d2506425a1a57a14f0d63d41a9b6d26
Nick <thenickoftime.x@gmail.com>
2024-04-15 22:16:30 +0000

    Adding resume files & to navigation

---
+++ src/pages/resume/resume.tsx
@@ -0,0 +1,11 @@
+import './resume.scss';
+
+export default function Resume() {
+	return (
+		<div id='resume'>
+			<h1>Hello World</h1>
+			<div id='resume-download'></div>
+			<div id='resume-container'></div>
+		</div>
+	);
+}
