13b4753a9c5a011685a3388b26083f59ffef8164
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-04-19 20:29:09 +0000

    style: adjusting styling and structure for certain components so an empty browser is still full page height with the footer at the bottom

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,14 +3,7 @@ import './footer.scss';
 export default function Footer() {
 	return (
 		<div id='footer'>
-			{/* <div id='footer-main'>
-				<div id='footer-hero'>
-					<img src='/hero.png' alt='Footer Hero' />
-				</div>
-			</div> */}
-			<div id='footer-background'>
-				<div id='footer-copyright'>{new Date().getFullYear()} © Nick Cunningham</div>
-			</div>
+			<div id='footer-copyright'>{new Date().getFullYear()} © Nick Cunningham</div>
 		</div>
 	);
 }
