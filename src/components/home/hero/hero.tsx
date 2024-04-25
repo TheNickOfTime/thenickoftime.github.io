63fdc96bc31f38fea4af11673a05607415cac12a
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-04-25 20:03:48 +0000

    refactor: revising hero structure and styling

--- src/components/home/hero/hero.tsx
+++ src/components/home/hero/hero.tsx
@@ -4,11 +4,17 @@ export default function Hero() {
 	return (
 		<div id='hero'>
 			<div id='hero-background' />
-			<img id='hero-image' src='/hero.png' alt='hero-portrait' />
 			<div id='hero-foreground'>
-				<div id='inner'>
+				<div id='hero-text'>
 					<div id='primary'>Hello there!</div>
 					<div id='secondary'>I'm Nick, a Developer.</div>
+				</div>
+				<div id='hero-image'>
+					<img
+						src='/profile/profile_transparent.png'
+						alt='hero-portrait'
+						loading='eager'
+					/>
 				</div>
 			</div>
 		</div>
